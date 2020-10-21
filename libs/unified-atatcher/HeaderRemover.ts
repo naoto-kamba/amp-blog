91efd41b944fd02337ca47d0cc5b451cc246af23
naoto-kamba <ayataka.nk@gmail.com>
2020-10-21 22:31:47 +0900

    md->html変換時に削るh1を最初の一つだけにした

--- libs/unified-atatcher/HeaderRemover.ts
+++ libs/unified-atatcher/HeaderRemover.ts
@@ -1,34 +1,40 @@
 export const headerRemover = () => {
   return function (node, vfile, next) {
     try {
-      const newNode = visit(convertCode, node)
-      next(null, newNode)
+      for (let i = 0; i < node.children.length; i++) {
+        if (node.children[i].tagName === "h1") {
+          node.children.splice(i, 1)
+          next(null, node)
+        }
+      }
+
+      next(null, node)
     } catch (err) {
       next(err)
     }
   }
 }
 
-const convertCode = (node) => {
-  if (node.tagName === "h1") {
-    return null
-  } else {
-    return node
-  }
-}
+// const convertCode = (node) => {
+//   if (node.tagName === "h1") {
+//     return null
+//   } else {
+//     return node
+//   }
+// }
 
-// hastの要素を訪問する関数
-const visit = (visitor, node) => {
-  const newNode = visitor(node)
-  if (newNode && newNode.children) {
-    const newChildren = []
-    for (let i = 0; i < newNode.children.length; i++) {
-      const result = visit(visitor, newNode.children[i])
-      if (result !== null) {
-        newChildren.push(result)
-      }
-    }
-    newNode.children = newChildren
-  }
-  return newNode
-}
+// // hastの要素を訪問する関数
+// const visit = (visitor, node) => {
+//   const newNode = visitor(node)
+//   if (newNode && newNode.children) {
+//     const newChildren = []
+//     for (let i = 0; i < newNode.children.length; i++) {
+//       const result = visit(visitor, newNode.children[i])
+//       if (result !== null) {
+//         newChildren.push(result)
+//       }
+//     }
+//     newNode.children = newChildren
+//   }
+//   return newNode
+// }
