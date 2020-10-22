66e1f2eefdf726aead676835ba152d045bf814dc
naoto-kamba <ayataka.nk@gmail.com>
2020-10-22 21:38:58 +0900

    Amp以外でlogoが表示されない不具合修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
 import React from "react"
+import { useAmp } from "next/amp"
 
 export const Header: React.FC = (props) => {
+  const isAmp = useAmp()
   return (
     <div className="root">
-      <amp-img src="/images/logo.png" width="307px" height="100px" />
+      {isAmp ? (
+        <amp-img src="/images/logo.png" width="307px" height="100px" />
+      ) : (
+        <img src="/images/logo.png" width="307px" height="100px" />
+      )}
       <style jsx>{`
         .root {
           box-sizing: border-box;
