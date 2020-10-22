5281cc3c5d970d39897f7a06678f867df13458d9
naoto-kamba <ayataka.nk@gmail.com>
2020-10-22 21:50:31 +0900

    企業ページへのリンクを作成

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,11 +1,28 @@
 import React from "react"
 import { ArticleTags } from "./article"
+import { useAmp } from "next/amp"
 
 export const Navigation: React.FC<{ tags: string[] }> = (props) => {
+  const isAmp = useAmp()
   return (
     <div>
-      <div>Navigation!!!!!</div>
+      <div className="dcomlink-wrap">
+        <a href="http://www.dcom-web.co.jp/">
+          {isAmp ? (
+            <amp-img src="/images/dcomlink.png" width="234px" height="129px" />
+          ) : (
+            <img src="/images/dcomlink.png" width="234px" height="129px" />
+          )}
+        </a>
+      </div>
+
       <ArticleTags tags={props.tags} />
+      <style jsx>{`
+        .dcomlink-wrap {
+          text-align: center;
+          margin-bottom: 20px;
+        }
+      `}</style>
     </div>
   )
 }
